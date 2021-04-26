127ab9d3d5763167b4b14e160e0d3f16d1d69c75
chikara-dot <victorycastlepower0209@gmail.com>
2021-04-26 06:32:44 +0000

    Finish

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7446203a76a879273cb963244b4ca7f02d372171f5f1715d980cfe0ad4c45f7217764eb9377af9f6f71fef1368bd39610d74e4a0171a94a39248df35d23c65d1'
